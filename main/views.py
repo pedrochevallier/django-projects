c28128862ac786ed6f83b3157a3a6f718defac65
Pedro Chevallier <pedrochevallier@gmail.com>
2024-05-28 12:38:31 +0200

    Fixed cards

--- main/views.py
+++ main/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 
 def cards_view(request):
     cards = [
-        {'title': 'Card 1', 'link': '/qr', 'description': 'This is the first card.'},
-        {'title': 'Card 2', 'link': '/url', 'description': 'This is the second card.'},
-        {'title': 'Card 3', 'link': '/game', 'description': 'This is the third card.'}
+        {'title': 'QR Code Generator', 'link': '/qr'},
+        {'title': 'URL Shortener', 'link': '/url'},
+        {'title': 'Map Game', 'link': '/game'}
     ]
     context = {'cards': cards}
     return render(request, 'cards_template.html', context)
